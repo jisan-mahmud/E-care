45d4c92819e8354f33d7e55b13e82f2348a798e5
kawsarkabir <devkawsarkabir@gmail.com>
2024-04-20 02:13:10 +0600

    slider done with dynamicaly also design

--- js/app.js
+++ js/app.js
@@ -1,46 +1,42 @@
-// Fetch services data
 const services_load = () => {
-  fetch("https://testing-8az5.onrender.com/services/")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data); // Log API response
-      display_service(data);
-    })
-    .catch((err) => console.log(err));
-};
+    fetch("https://testing-8az5.onrender.com/services/")
+      .then((res) => res.json())
+      .then((data) => { 
+        display_service(data);
+      })
+      .catch((err) => console.log(err));
+  };
 
-// Display services
-const display_service = (services) => {
+  // display services
+  const display_service = (services) => {
     const glideContainer = document.querySelector(".swiper-wrapper");
-  
+
     services.forEach((service) => {
       const slide = document.createElement("div");
-      slide.className = "swiper-slide flex flex-col justify-center items-center";
+      slide.className = "swiper-slide";
+      slide.style.backgroundImage = `url(${service.image})`;
       slide.innerHTML = `
-        <div class="flex flex-col justify-center items-center">
-          <img src="${service.image}" class="w-full h-52 mb-4" />
-          <div class="text-center">
-            <h3 class="text-xl font-semibold">${service.name}</h3>
-            <p class="text-sm">${service.description}</p>
-          </div>
+        <div class="slide-content">
+          <h3>${service.name}</h3>
+          <p>${service.description}</p>
         </div>
       `;
       glideContainer.appendChild(slide);
     });
+
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
   };
-  
 
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-services_load();
+  services_load();
