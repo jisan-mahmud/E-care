14370feda4168fdc47ab61bfc0b58b389d1c4fb0
kawsarkabir <devkawsarkabir@gmail.com>
2024-04-20 03:02:23 +0600

    slice see more btn added done from me

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// data load
 const services_load = () => {
   fetch("https://testing-8az5.onrender.com/services/")
     .then((res) => res.json())
@@ -17,22 +18,42 @@ const display_service = (services) => {
     slide.style.backgroundImage = `url(${service.image})`;
 
     const sliceDesc = service.description.split(" ").slice(0, 20).join(" ");
+    const fullDesc = service.description;
 
     slide.innerHTML = `
         <div class="slide-content">
           <h3>${service.name}</h3>
           <p class="description">${sliceDesc}</p>
-          <button class="see-more-btn">See More</button>
+          <button class="see-more-btn text-blue-700 border-blue-700 border px-3 mt-4">See More</button>
+          <p class="full-description hidden">${fullDesc}</p>
         </div>
       `;
     glideContainer.appendChild(slide);
+  });
+
+  //  see more btn event handler
+  const seeMoreButtons = document.querySelectorAll(".see-more-btn");
+  seeMoreButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const fullDesc = this.parentElement.querySelector(".full-description");
+      const sliceDesc = this.parentElement.querySelector(".description");
+
+      if (fullDesc.classList.contains("hidden")) {
+        fullDesc.classList.remove("hidden");
+        sliceDesc.classList.add("hidden");
+        this.textContent = "See Less";
+      } else {
+        fullDesc.classList.add("hidden");
+        sliceDesc.classList.remove("hidden");
+        this.textContent = "See More";
+      }
+    });
   });
 };
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 20,
-  // centeredSlides: true,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
